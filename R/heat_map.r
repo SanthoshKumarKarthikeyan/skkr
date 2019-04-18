7597ed14a65700d6e5eaa53758c9810c3e15d724
Santhosh Kumar Karthikeyan <46432913+SanthoshKumarKarthikeyan@users.noreply.github.com>
2019-04-17 22:04:46 -0400

    Update heat_map.r

--- R/heat_map.r
+++ R/heat_map.r
@@ -5,6 +5,7 @@ heat_map <- function(b)
   #Heatmap with pearson and spearman dendogram
   hr <- hclust(as.dist(1-cor(t(b), method="pearson")), method="complete")
   hc <- hclust(as.dist(1-cor(b, method="spearman")), method="complete")
+  #Visualize the data with the heatmap, and the dendograms
   d <- heatmap(b,col=brewer.pal(10,"Blues"), Rowv=as.dendrogram(hr), Colv=as.dendrogram(hc), scale="row", density.info="none", trace="none")
 }
 
