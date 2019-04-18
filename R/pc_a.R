b21dad08c801f2b2e9b56c7ae14275bf57394679
Santhosh Kumar Karthikeyan <46432913+SanthoshKumarKarthikeyan@users.noreply.github.com>
2019-04-17 22:01:13 -0400

    Update pc_a.R

--- R/pc_a.R
+++ R/pc_a.R
@@ -1,9 +1,14 @@
 pc_ana <- function(b)
 {
   library(factoextra)
+  #importing factoextra
   s <- prcomp(b, center = TRUE,scale. = TRUE)
   print(s)
   fviz_pca_ind(s,
                col.ind = "cos2",
                repel = TRUE)
 }
+
+#Examples
+#mtcars <- as.matrix(mtcars)
+#pc_ana(mtcars)
